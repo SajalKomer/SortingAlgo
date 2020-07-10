5ca349a3a267d1ef1c734b07af143eb6647aab16
Shree <sajalkomer@gmail.com>
2020-07-10 03:24:26 -0400

    added StringReverser class

--- src/sortingPackage/Main.java
+++ src/sortingPackage/Main.java
@@ -3,20 +3,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.addLast(40);
-        list.addLast(50);
-        list.addLast(60);
-        list.addLast(70);
+        String str = "abcd";
 
-        //var kth = list.getKthFromTheEnd(4);
-        //System.out.println(kth);
-        list.printMiddle();
-
-        var list2 = LinkedList.createWithLoop();
-        System.out.println(list2.hasLoop());
+        StringReverser reverser = new StringReverser();
+        var result = reverser.reverse(str);
+        System.out.println(result);
     }
 }
